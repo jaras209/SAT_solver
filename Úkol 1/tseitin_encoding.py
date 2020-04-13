27aa6aedd5145a1cb11c12d36ed6779e08e86f33
Jaroslav <safarjaroslav209@gmail.com>
2020-04-13 12:29:58 +0200

    Initial commit

---
+++ Úkol 1/tseitin_encoding.py
@@ -0,0 +1,17 @@
+import argparse
+
+
+class FormulaTree:
+    def __init__(self, root, left=None, right=None):
+        self.root = root
+        self.left = left
+        self.right = right
+    
+if __name__ == "__main__":
+    # Parse arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input_file", default=None, type=str, help="Input file which contains a description of a single formula in NNF")
+    args = parser.parse_args()
+
+    with open(args.input_file, mode="r") as input_file:
+        
